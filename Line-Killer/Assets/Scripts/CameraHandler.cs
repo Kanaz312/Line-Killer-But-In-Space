7f12663345345f25bb0fb0344f1d387f57e9dcad
Giovanni <41044148+GiovanniLibrizzi@users.noreply.github.com>
2022-11-11 18:38:26 -0800

    Comment camera handler, add escape to quit

--- Line-Killer/Assets/Scripts/CameraHandler.cs
+++ Line-Killer/Assets/Scripts/CameraHandler.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class CameraHandler : MonoBehaviour {
     private Camera _cam;
-    private BoxCollider2D _cameraBox;
-    [SerializeField] float zVal = -10f;
+    private LineHandler _lineHandler = null;
 
-    [SerializeField] private LineHandler _lineHandler = null;
-    [SerializeField] private float padding = 1f;
-    [SerializeField] private float zoomSpd = 0.1f;
-    private float camSize;
+    [SerializeField] float zVal = -10f; // The z position of the camera
+    [SerializeField] private float padding = 1f;    // The padding around the polygon
+    [SerializeField] private float zoomSpd = 0.1f;  // How fast it lerps
 
     // Start is called before the first frame update
     void Start()
     {
         _cam = gameObject.GetComponent<Camera>();
-        _cameraBox = _cam.GetComponent<BoxCollider2D>();
-        
-
         _lineHandler = FindObjectOfType<LineHandler>();
 
     }
@@ -26,32 +21,32 @@ public class CameraHandler : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        // TODO: Make this a co-routine that's only run when a slice is called
+        ScaleScreen();
+    }
+
+
+    // Scales the screen when called
+    private void ScaleScreen() {
+        // Get the bounds of the polygon
         Rect polyRect = _lineHandler.GetPointBounds();
-        Debug.Log("Width: " + polyRect.width.ToString() + " || Height: " + polyRect.height.ToString()); // + " " + polyRect.yMin.ToString() + " " + polyRect.yMax.ToString());
-        //Debug.Log()
-        //_cam.rect = new Rect(
-        //    polyRect.xMin,
-        //    polyRect.yMax,
-        //    polyRect.width,
-        //    polyRect.height
 
-        //    );
-
-
-        //    )
-       // polyRect.xMin - padding - (polyRect.width/2) + (width/2)
-                        
-        if (polyRect.width > (polyRect.height * _cam.aspect) + 2 ) {
+        //Debug.Log("Width: " + polyRect.width.ToString() + " || Height: " + polyRect.height.ToString()); // + " " + polyRect.yMin.ToString() + " " + polyRect.yMax.ToString());
+        float camSize;
+        // Scale whether it's wider or smaller with respect to the aspect ratio
+        if (polyRect.width > (polyRect.height * _cam.aspect) + padding) {
             camSize = ((polyRect.width / 2) / _cam.aspect) + padding;
-        } else { 
+        } else {
             camSize = (polyRect.height / 2) + padding;
         }
 
+        // Smooth the camera scaling
         _cam.orthographicSize = Mathf.Lerp(_cam.orthographicSize, camSize, zoomSpd);
 
-        float height = 2f * _cam.orthographicSize;
-        float width = height * _cam.aspect;
-
-        transform.position = new Vector3(polyRect.xMin + (polyRect.width / 2), polyRect.yMax - (polyRect.height/2), zVal);
+        // Center the polygon
+        Vector3 camPos = new Vector3(polyRect.xMin + (polyRect.width / 2), polyRect.yMax - (polyRect.height / 2), zVal);
+        // Smooth the camera transform
+        transform.position = Vector3.Lerp(transform.position, camPos, zoomSpd);
     }
+
 }
